dbe241605a209ad5d5cbe0734527410276858b3e
JaeWon Baek <bjw1622@gmail.com>
2022-08-08 11:35:51 +0900

    Add User Entity, Controller

---
+++ Entitys/UserEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Board.Entitys
+{
+    public class UserEntity
+    {
+        public string Email { get; set; }
+        public string Pw { get; set; }
+        public string Name { get; set; }
+        public string Birth { get; set; }
+    }
+}
