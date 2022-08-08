4d4e755ae0e182bacfbee339fb449fb6f5a679cf
JaeWon Baek <bjw1622@gmail.com>
2022-08-08 15:19:38 +0900

    로그인 페이지 controller 추가

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Board.Entitys;
+using Board.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,30 @@ namespace Board.Controllers
 {
     public class UserController : Controller
     {
+        User user = new User();
         // GET: User
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult AddUsers(UserEntity obj)
+        {
+            user.AddUser(obj);
+            return View();
+        }
+
+        //이메일 중복 체크
+        //public ActionResult CheckEmail(string email)
+        //{
+        //    user.CheckEmail(email);
+        //    return View();
+        //}
+
+        public ActionResult Login()
+        {
+            return View();
+        }
     }
 }
+
---
+++ Models/User.cs
@@ -0,0 +1,55 @@
+﻿using Board.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Board.Models
+{
+    public class User
+    {
+        // sqlConnection 
+        private SqlConnection con;
+
+        public void Conn()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["BoardDB"].ToString();
+            con = new SqlConnection(constr);
+        }
+
+        public void AddUser(UserEntity obj)
+        {
+            Conn();
+            con.Open();
+            // 사용할 프로시저의 이름을 설정
+            using (SqlCommand com = new SqlCommand("dbo.InsertUser", con))
+            {
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@Email", obj.Email);
+                com.Parameters.AddWithValue("@Pw", obj.Pw);
+                com.Parameters.AddWithValue("@Name", obj.Name);
+                com.Parameters.AddWithValue("@Birth", obj.Birth);
+                com.ExecuteNonQuery();
+            }
+            con.Close();
+            con.Dispose();
+        }
+        //public void CheckEmail(string email)
+        //{
+        //    Conn();
+        //    con.Open();
+        //    // 사용할 프로시저의 이름을 설정
+        //    using (SqlCommand com = new SqlCommand("dbo.emailCheck", con))
+        //    {
+        //        com.CommandType = CommandType.StoredProcedure;
+        //        var num = com.ExecuteNonQuery();
+        //        Console.WriteLine(num);
+        //    }
+        //    con.Close();
+        //    con.Dispose();
+        //}
+    }
+}
