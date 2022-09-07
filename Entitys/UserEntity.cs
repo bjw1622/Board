a0a6ec653d8af1f5ca23c926a56cd153ee0a71b3
JaeWon Baek <bjw1622@gmail.com>
2022-09-07 17:05:42 +0900

    Annotation 제거

--- Entitys/UserEntity.cs
+++ Entitys/UserEntity.cs
@@ -8,14 +8,10 @@ namespace Board.Entitys
 {
     public class UserEntity
     {
-        [Required]
         public int No { get; set; }
-        [Required]
         public string Email { get; set; }
         public string Pw { get; set; }
-        [Required]
         public string Name { get; set; }
-        [Required]
         public string Birth { get; set; }
         public string CreateDate { get; set; }
     }
